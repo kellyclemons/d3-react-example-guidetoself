bc20171d5404b717103bcf6a8509c00f3842d784
Kelly Clemons <kelly.clemons@mac.com>
2017-12-18 13:02:39 -0600

    imported newly created BarChart component and passed down data and size to component

--- src/BarChart.js
+++ src/BarChart.js
@@ -42,7 +42,7 @@ class BarChart extends Component {
       .data(this.props.data)
       .style('fill', '#fe9922')
       .attr('x', (d,i) => i * 25)
-      .attr('y', d => this.props.size[1] — yScale(d))
+      .attr('y', d => this.props.size[1] - yScale(d))
       .attr('height', d => yScale(d))
       .attr('width', 25)
 
